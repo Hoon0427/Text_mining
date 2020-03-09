e2b88d6cff3942206b9e7477a4b5dfd57458d9d6
Hoon0427 <ikaros977@gmail.com>
2020-03-09 12:59:20 +0900

    write_01

---
+++ Text_mining_01.R
@@ -0,0 +1,10 @@
+text <- c("Because I could not stop for Death -",
+          "He kindly stopped for me -",
+          "The Carriage held but just Ourselves -",
+          "and Immrtality")
+text
+
+library(dplyr)
+text_df <- data_frame(line = 1:4, text = text)
+
+text_df
