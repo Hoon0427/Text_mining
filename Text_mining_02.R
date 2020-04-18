77e03fe0f9e33bd4f8bfb08f89bf2f3ab62c6291
Hoon0427 <ikaros977@gmail.com>
2020-04-18 23:34:17 +0900

    study

--- Text_mining_02.R
+++ Text_mining_02.R
@@ -8,3 +8,20 @@ sentiments
 get_sentiments("afinn")
 get_sentiments("bing")
 get_sentiments("nrc")
+
+#install.packages("janeaustenr")
+
+library(janeaustenr)
+library(dplyr)
+library(stringr)
+
+tidy_books <- austen_books() %>% 
+  group_by(book) %>% 
+  mutate(linenumber = row_number(),
+         chapter = cumsum(str_detect(text, regex("^chapter [\\divxlc]",
+                                                 ignore_case = TRUE)))) %>% 
+  ungroup() %>% 
+  unnest_tokens(word, text)
+
+nrcjoy <- get_sentiments("nrc") %>% 
+  filter(sentiments == "joy")
